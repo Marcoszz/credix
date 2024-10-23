ac350c11a368e6b5887e80442f0da542eb6ba32f
Marcos Lopes <marcos.mpdcl@gmail.com>
2024-10-22 23:51:10 -0300

    feat: Enabling cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -5,6 +5,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  })
+
   const config = new DocumentBuilder()
     .setTitle('E-commerce API')
     .setDescription('E-commerce API with CrediPay')
